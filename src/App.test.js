2da2b39374d09de096cac34d65990d6dfdbc1ffc
esojlm5 <ne.ver1@outlook.com>
2020-09-20 01:13:28 -0500

    remove not use

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import ListResults from "./ListResults";
-
-test("render ListResult", () => {
-  const { container, getByText } = render(<ListResults />);
-
-  expect(container.firstChild).toMatchSnapshot();
-});
